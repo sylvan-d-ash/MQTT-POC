6d20040d580be9254a8be7a55abb796cdbc2263c
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-08-29 12:25:01 +0300

    Initial Commit

---
+++ MQTT POC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MQTT POC
+//
+//  Created by Sylvan  on 29/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MQTT POC/MQTT_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MQTT_POCApp.swift
+//  MQTT POC
+//
+//  Created by Sylvan  on 29/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MQTT_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
